363f1fd627e3ed4fb8778b61665d86d1087ec206
ditzzzXZ <ditzzz.ofc@gmail.com>
2025-11-04 14:37:55 +0700

    Add SoundCloud URL prop to Ipod component

--- app/page.tsx
+++ app/page.tsx
@@ -14,6 +14,7 @@ export default async function Page({
     <Ipod
       spotifyCallbackCode={spotifyCode}
       appleAccessToken={appleAccessToken}
+      soundcloudUrl={soundcloudUrl}
     />
   );
 }
